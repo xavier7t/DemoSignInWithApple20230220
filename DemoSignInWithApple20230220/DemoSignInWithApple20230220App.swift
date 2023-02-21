acb82d61d102e2a2da5ffee6c7c5a7bb6ef3245e
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-20 22:27:23 -0500

    Initial Commit

---
+++ DemoSignInWithApple20230220/DemoSignInWithApple20230220App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSignInWithApple20230220App.swift
+//  DemoSignInWithApple20230220
+//
+//  Created by Xavier on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoSignInWithApple20230220App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
