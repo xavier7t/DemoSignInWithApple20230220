2ce71aa151d5d198d47f903b48004b27b2cd3bd2
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-20 23:53:32 -0500

    DemoSignInWithApple20230220

--- DemoSignInWithApple20230220/ContentView.swift
+++ DemoSignInWithApple20230220/ContentView.swift
@@ -29,7 +29,25 @@ struct ContentView: View {
                     //Handle auth failures
                 }
             }
+            .frame(width: 280, height: 40, alignment: .center)            .signInWithAppleButtonStyle(.black)
+            
+            Spacer()
+                .frame(height: 20)
+            
+            SignInWithAppleButton(.signUp) { request in
+                request.requestedScopes = [.fullName, .email]
+            } onCompletion: { result in
+                switch result {
+                case .success(let authResult):
+                    print("Auth success. Result: \(authResult)")
+                    //Post-authentication updates on persistence and/or states.
+                case .failure(let error):
+                    print("Auth failed. Result: \(error.localizedDescription)")
+                    //Handle auth failures
+                }
+            }
             .frame(width: 280, height: 40, alignment: .center)
+            .signInWithAppleButtonStyle(.whiteOutline)
         }
         .padding()
     }
